ff40411f00faf8218175eee8a622c8ebc0a245d5
Qichao Dong <qichaodong@gmail.com>
2012-04-20 00:58:16 -0700

    Add alternative sort-by field - name, in addition to score.

--- leaderboard.js
+++ leaderboard.js
@@ -4,8 +4,13 @@
 Players = new Meteor.Collection("players");
 
 if (Meteor.is_client) {
+  Meteor.startup(function(){
+    Session.set("is_sorting_by_score", true);
+  });
+
   Template.leaderboard.players = function () {
-    return Players.find({}, {sort: {score: -1, name: 1}});
+    return Players.find({}, {sort: Session.get("is_sorting_by_score") ?
+      {score: -1, name: 1} : {name: 1}});
   };
 
   Template.leaderboard.selected_name = function () {
@@ -13,11 +18,25 @@ if (Meteor.is_client) {
     return player && player.name;
   };
 
+  Template.leaderboard.next_sort_by_field = function () {
+    return (Session.get("is_sorting_by_score") ?
+      "Sort by name" : "Sort by score");
+  };
+
+  Template.leaderboard.current_sort_by_field = function () {
+    return (Session.get("is_sorting_by_score") ?
+      "score" : "name");
+  };
+
   Template.player.selected = function () {
     return Session.equals("selected_player", this._id) ? "selected" : '';
   };
 
   Template.leaderboard.events = {
+    'click input.sort_by': function(){
+        Session.set("is_sorting_by_score",
+        !Session.get("is_sorting_by_score"));
+    },
     'click input.inc': function () {
       Players.update(Session.get("selected_player"), {$inc: {score: 5}});
     }
@@ -35,13 +54,11 @@ if (Meteor.is_server) {
   Meteor.startup(function () {
     if (Players.find().count() === 0) {
       var names = ["Ada Lovelace",
-                   "Sophia Rolland",
                    "Grace Hopper",
                    "Marie Curie",
                    "Carl Friedrich Gauss",
                    "Nikola Tesla",
-                   "Claude Shannon",
-                   "Daisy Tang"];
+                   "Claude Shannon"];
       for (var i = 0; i < names.length; i++)
         Players.insert({name: names[i], score: Math.floor(Math.random()*10)*5});
     }
